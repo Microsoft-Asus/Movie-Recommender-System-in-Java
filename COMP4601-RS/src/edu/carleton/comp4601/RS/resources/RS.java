22e438a7aa148630bce2cbbe21546730f409c6c7
julianClayton <julian.clayton@graphitesoftware.com>
2018-03-31 17:11:33 -0400

    DYNAMIC ADVERTISEMENTS

--- COMP4601-RS/src/edu/carleton/comp4601/RS/resources/RS.java
+++ COMP4601-RS/src/edu/carleton/comp4601/RS/resources/RS.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.List;
+import java.util.Random;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -166,13 +168,18 @@ public class RS {
 		ArrayList<Advertisement> ads = user.getClosestAdvertisements(1);
 		StringBuilder listhtml = new StringBuilder();
 		String html = Lucene.getInstance().queryPage(pageid);
-		System.out.println(html);
+		Random randomGenerator = new Random();
+		int numGenresForAd = randomGenerator.nextInt(ads.get(0).getGenres().length);
+		List<String> genresRandom = Arrays.asList(ads.get(0).getGenres().clone());
+		Collections.shuffle(genresRandom);
 		for (Advertisement ad : ads) {
 			listhtml.append("<li>");
-			listhtml.append(ad.getGenres()[0]);
-			for (int i = 1; i < ad.getGenres().length;i++) {
-				listhtml.append(", " + ad.getGenres()[i]);
+			listhtml.append(genresRandom.get(0));
+			
+			for (int i = 1; i < numGenresForAd+1;i++) {
+				listhtml.append(", " + genresRandom.get(i));
 			}
+			listhtml.append(" advertisement");
 			listhtml.append("</li>");
 		}
 		return "<!DOCTYPE html><html><head><style>div.container {width: 100%;border: 1px solid gray;}header, footer {padding: 1em;color: white;background-color: black;clear: left;text-align: center;}nav {float: left;max-width: 160px;margin: 0;padding: 1em;}nav ul {list-style-type: none;padding: 0;}nav ul a {text-decoration: none;}article {margin-left: 170px;border-left: 1px solid gray;padding: 1em;overflow: hidden;}</style></head><body><div class=\"container\"><header><h1>Movie "+ pageid + "</h1></header><nav><ul>" + listhtml.toString() + "</ul></nav><article><h1>" + pageid + "</h1> <p>"+ html + "</p></article><footer></footer></div></body></html>";
